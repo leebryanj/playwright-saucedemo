93eb57f0a3308aa5c48609995c5092e98ab69644
leebryanj <41661178+leebryanj@users.noreply.github.com>
2026-08-19 17:41:43 -0700

    Add login tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,22 +1,80 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
-test('has title on login page', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Swag Labs/);
-});
-
-test('successful login', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-
-  // Fill the username with 'standard_user' and fill the password
-  await page.getByPlaceholder('Username').fill('standard_user');
-  await page.getByPlaceholder('Password').fill('secret_sauce');
-
-  // Click the Login button
+// Login helper function
+async function login(page: Page, username: string, password: string) {
+  await page.getByPlaceholder('Username').fill(username);
+  await page.getByPlaceholder('Password').fill(password);
   await page.getByRole('button', { name: 'Login' }).click();
+}
 
-  // Expects page to have a title with text 'Products'
-  await expect(page.locator('[data-test="title"]')).toHaveText('Products');
+test.describe('Login', () => {
+
+  // Start at the login page
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+  });
+
+  test('has title on login page', async ({ page }) => {
+    // Expect title to have Swag Labs substring
+    await expect(page).toHaveTitle(/Swag Labs/);
+  });
+
+  test('successful login with standard user', async ({ page }) => {
+    await login(page, 'standard_user', 'secret_sauce');
+
+    // Expects page to have updated URL and title with text 'Products'
+    await expect(page).toHaveURL(/inventory/);
+    await expect(page.locator('[data-test="title"]')).toHaveText('Products');
+  });
+
+  test('login fails with correct username and incorrect password', async ({ page }) => {
+    await login(page, 'standard_user', 'abc123');
+
+    // Both username and password fields display an error svg
+    const errorSVGs = page.locator('svg[data-icon="circle-xmark"]');
+    await expect(errorSVGs).toHaveCount(2);
+    await expect(errorSVGs.nth(0)).toBeVisible();
+    await expect(errorSVGs.nth(1)).toBeVisible();
+
+    await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
+  });
+
+  test('login fails with invalid username and correct password', async ({ page }) => {
+    await login(page, 'not_a_user', 'secret_sauce');
+
+    await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
+  });
+
+  test('login fails with locked out user', async ({ page }) => {
+    await login(page, 'locked_out_user', 'secret_sauce');
+
+    await expect(page.getByText('Epic sadface: Sorry, this user has been locked out.')).toBeVisible();
+  });
+
+  test('login fails with empty username', async ({ page }) => {
+    await login(page, '', 'secret_sauce');
+
+    // Username input field is still empty
+    await expect(page.getByPlaceholder('Username')).toHaveValue('');
+    await expect(page.getByText('Epic sadface: Username is required')).toBeVisible();
+  });
+
+  test('login fails with empty password', async ({ page }) => {
+    await login(page, 'standard_user', '');
+
+    await expect(page.getByPlaceholder('Password')).toHaveValue('');
+    await expect(page.getByText('Epic sadface: Password is required')).toBeVisible();
+  });
+
+  test('clicking "x" on error message toast removes error state', async ({ page }) => {
+    await login(page, 'standard_user', '');
+
+    // Check that error toast is present
+    await expect(page.getByText('Epic sadface: Password is required')).toBeVisible();
+
+    await page.locator('[data-test="error-button"]').click();
+
+    await expect(page.getByText('Epic sadface: Password is required')).not.toBeVisible();
+  });
 });
+

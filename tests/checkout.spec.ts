30bd33a274344d3b0d3081f2c4b0707c635115e9
leebryanj <41661178+leebryanj@users.noreply.github.com>
2026-08-27 20:45:18 -0700

    Add additional Checkoout tests

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -85,26 +85,92 @@ test.describe('Checkout', () => {
     });
 
     test('Leaving all fields blank shows correct error', async ({ page }) => {
+        // Reach the Checkout Your Information page
+        await page.locator('[data-test="shopping-cart-link"]').click();
+        await page.locator('[data-test="checkout"]').click();
 
+        // Leave all fields blank and click continue button
+        await page.locator('[data-test="continue"]').click();
+
+        // Shows the error message for first field left blank
+        await expect(page.locator('[data-test="error"]')).toHaveText('Error: First Name is required');
     });
 
     test('Leaving first name field blank shows correct error', async ({ page }) => {
+        // Reach the Checkout Your Information page
+        await page.locator('[data-test="shopping-cart-link"]').click();
+        await page.locator('[data-test="checkout"]').click();
 
+        // Fill in fields except First Name and click continue button
+        await page.locator('[data-test="lastName"]').fill('User');
+        await page.locator('[data-test="postalCode"]').fill('A1A 1A1');
+        await page.locator('[data-test="continue"]').click();
+
+        // Shows the error message for first field left blank
+        await expect(page.locator('[data-test="error"]')).toHaveText('Error: First Name is required');
     });
 
     test('Leaving last name field blank shows correct error', async ({ page }) => {
+        // Reach the Checkout Your Information page
+        await page.locator('[data-test="shopping-cart-link"]').click();
+        await page.locator('[data-test="checkout"]').click();
 
+        // Fill in fields except Last Name and click continue button
+        await page.locator('[data-test="firstName"]').fill('Test');
+        await page.locator('[data-test="postalCode"]').fill('A1A 1A1');
+        await page.locator('[data-test="continue"]').click();
+
+        // Shows the error message for first field left blank
+        await expect(page.locator('[data-test="error"]')).toHaveText('Error: Last Name is required');
     });
 
     test('Leaving zip/postal code field blank shows correct error', async ({ page }) => {
+        // Reach the Checkout Your Information page
+        await page.locator('[data-test="shopping-cart-link"]').click();
+        await page.locator('[data-test="checkout"]').click();
 
+        // Fill in fields except Last Name and click continue button
+        await page.locator('[data-test="firstName"]').fill('Test');
+        await page.locator('[data-test="lastName"]').fill('User');
+        await page.locator('[data-test="continue"]').click();
+
+        // Shows the error message for first field left blank
+        await expect(page.locator('[data-test="error"]')).toHaveText('Error: Postal Code is required');
     });
 
     test('Clicking cancel in checkout takes user back to Product page', async ({ page }) => {
+        // Reach the Checkout Your Information page
+        await page.locator('[data-test="shopping-cart-link"]').click();
+        await page.locator('[data-test="checkout"]').click();
 
+        // Click the cancel button
+        await page.locator('[data-test="cancel"]').click();
+
+        // Check that we are back to the Your Cart page
+        await expect(page.locator('[data-test="title"]')).toHaveText('Your Cart');
     });
 
     test('Checkout page shows correct item list', async ({ page }) => {
+        // Add an item to Cart and save product information
+        const sauceLabsBackpack = page.locator('[data-test="inventory-item"]').filter({ hasText: 'Sauce Labs Backpack' });
+        const productName = await sauceLabsBackpack.locator('[data-test="inventory-item-name"]').innerText();
+        const productDescription = await sauceLabsBackpack.locator('[data-test="inventory-item-desc"]').innerText();
+        const productPrice = await sauceLabsBackpack.locator('[data-test="inventory-item-price"]').innerText();
 
+        // Add that item to cart
+        await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+        // Reach Checkout
+        await page.locator('[data-test="shopping-cart-link"]').click();
+        await page.locator('[data-test="checkout"]').click();
+        await page.locator('[data-test="firstName"]').fill('Test');
+        await page.locator('[data-test="lastName"]').fill('User');
+        await page.locator('[data-test="postalCode"]').fill('A1A 1A1');
+        await page.locator('[data-test="continue"]').click();
+
+        // Confirm Checkout item details matches the Product page details
+        await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText(productName);
+        await expect(page.locator('[data-test="inventory-item-desc"]')).toHaveText(productDescription);
+        await expect(page.locator('[data-test="inventory-item-price"]')).toHaveText(productPrice);
     });
 });
